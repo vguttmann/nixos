246f38f05bfe13100043fb41a41fc1b2da4a05fb
vguttmann <vincent.guttmann@gmx.de>
2025-09-02 11:20:02 +0000

    Test if there's a conflict with nvim-nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    nixvim.url = github:jkulzer/nvim-nix;
+#    nixvim.url = github:jkulzer/nvim-nix;
     config-nix-nvim.url = github:vguttmann/config-nix.nvim;
   };
-  outputs = { self, nixpkgs, nixvim, config-nix-nvim }: {
+  outputs = { self, nixpkgs, config-nix-nvim }: {
+#  outputs = { self, nixpkgs, nixvim, config-nix-nvim }: {
     # replace 'joes-desktop' with your hostname here.
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [ ./configuration.nix ];
@@ -12,9 +13,9 @@
       pkgs = import nixpkgs {
         overlays = [
           config-nix-nvim.overlays.default
-          (final: prev: {
-            neovim = nixvim.packages.x86_64-linux.default;
-          })
+#          (final: prev: {
+#            neovim = nixvim.packages.x86_64-linux.default;
+#          })
         ];
       };
 
