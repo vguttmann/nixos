374d3d635db00d8eb14d98163992eb970c5d59cb
vguttmann <vincent.guttmann@gmx.de>
2025-09-02 11:04:48 +0000

    Added kickstart-nvim

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,7 @@
   ];
 
   environment.systemPackages = with pkgs; [
+    nvim-pkg
     tmux
     python3
     fzf
--- flake.nix
+++ flake.nix
@@ -2,16 +2,16 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     nixvim.url = github:jkulzer/nvim-nix;
+    config-nix-nvim.url = github:vguttmann/config-nix.nvim;
   };
-  outputs = { self, nixpkgs, nixvim }: {
+  outputs = { self, nixpkgs, nixvim, config-nix-nvim }: {
     # replace 'joes-desktop' with your hostname here.
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [ ./configuration.nix ];
       system = "x86_64-linux";
-
-
       pkgs = import nixpkgs {
         overlays = [
+          config-nix-nvim.overlays.default
           (final: prev: {
             neovim = nixvim.packages.x86_64-linux.default;
           })
