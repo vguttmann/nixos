5ac6893976e98f8b8e933c6206607886bf7dd7cc
vguttmann <vincent.guttmann@gmx.de>
2025-09-10 08:25:37 +0000

    Add VSCode Server

--- flake.nix
+++ flake.nix
@@ -1,21 +1,22 @@
 {
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-#    nixvim.url = github:jkulzer/nvim-nix;
     config-nix-nvim.url = github:vguttmann/config-nix.nvim;
+    vscode-server.url = github:nix-community/nixos-vscode-server;
   };
-  outputs = { self, nixpkgs, config-nix-nvim }: {
-#  outputs = { self, nixpkgs, nixvim, config-nix-nvim }: {
-    # replace 'joes-desktop' with your hostname here.
+  outputs = { self, nixpkgs, config-nix-nvim, vscode-server }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+        vscode-server.nixosModules.default
+        ({ config, pkgs, ... }: {
+          services.vscode-server.enable = true;
+        })
+      ];
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         overlays = [
           config-nix-nvim.overlays.default
-#          (final: prev: {
-#            neovim = nixvim.packages.x86_64-linux.default;
-#          })
         ];
       };
 
